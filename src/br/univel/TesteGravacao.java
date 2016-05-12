834dfa43972a19b3dc5ef8e5907c8b2c301ce28f
fdsjunior <fdsjunior@WS-LAB02_125.univel.univel.br>
2016-05-11 22:17:09 -0300

    Primeiro Commit

---
+++ src/br/univel/TesteGravacao.java
@@ -0,0 +1,15 @@
+package br.univel;
+
+public class TesteGravacao {
+	
+	public static void main(String[] args) {
+		
+		Cliente c = new Cliente();
+		c.setId(1);
+		c.setNome("João");
+		
+		
+		
+	}
+
+}
